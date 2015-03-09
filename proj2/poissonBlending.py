9bfc175a5a4f1a71b8677bb89b2a748f5ddafa87
Mark Heimann <mheimann@wustl.edu>
2015-03-08 21:52:40 -0500

    solve the blending system of equations in poisson blending

--- proj2/poissonBlending.py
+++ proj2/poissonBlending.py
@@ -21,27 +21,80 @@ def poissonBlend(source, target, mask):
 	
 	#Step 2: solve blending constraints
 	#TODO: set source image (and region) and target image (and region) later
-	sourceArr = ndimage.imread('samples/toy_problem.png')
-	sourceWidth = sourceArr.shape[1]
-	sourceHeight = sourceArr.shape[0]
+	sourceWidth = source.shape[1]
+	sourceHeight = source.shape[0]
 	
-	targetArr = ndimage.imread('samples/toy_problem.png')
-	targetWidth = targetArr.shape[1]
-	targetHeight = targetArr.shape[0]
+	targetWidth = target.shape[1]
+	targetHeight = target.shape[0] 
+	
+	#set up least squares problem: Ax = b solving for x
+	#keep track of indices of nonnegative entries in what will be a sparse matrix
+	ArowIndices = []
+	AcolIndices = []
+	Adata = [] #list of equations for the vector A
+	b = list() #list of equations for the vector b
+	#later we can make these into sparse matrices
 	
 	#keep track of number of equations
 	e = 0
 	
-	#set up least squares problem: Ax = b solving for x
-	A = list() #list of equations for the vector A
-	b = list() #list of equations for the vector b
-	#later we can make these into sparse matrices
+	#EXPLORE: assign weighting factor for gradient constraint vs value constraint
+	#(instead of just 1s and -1's: equal weight)
+	
+	#EXPLORE: size of neighborhood: (4 pixel neighborhood versus 8 pixel neighborhood)
 	
 	#for each pixel in the source image
-	for i in range(0,sourceWidth):
-		for j in range(0,sourceHeight):
-			
-			e += 1
+	for i in range(0,sourceWidth): #column
+		for j in range(0,sourceHeight): #row
+			if mask[j,i]: #this pixel is in the source region
+				originalPixelValue = source[j,i]
+				#for each neighboring pixel in the source *that we haven't checked*
+				#don't check neighbors you've already checked 
+				#(e.g. comparing pixels [1,1] and [2,1] is same as comparing pixels [2,1] and [1,1])
+				for m in range(0,2):
+					for n in range(0,2):
+						#two conditions:
+						#either m or n is nonzero, so pixel's neighbor is distinct from it
+						#neighboring pixel is also in the source region
+						if m + n > 0 and mask[j+n,i+m]:
+							ArowIndices.append(e) #the e-th equation
+							AcolIndices.append(j*sourceWidth + i) #which entry
+							Adata.append(1) #v_i
+						
+							neighbor = source[j+n,i+m]
+							ArowIndices.append(e)
+							AcolIndices.append((j+n)*sourceWidth + (i+m))
+							Adata.append(-1) #-v_j
+						
+							b.append(neighbor - originalPixelValue)
+							e += 1
+						if m + n > 0 and not mask[j+n,i+m]:
+							ArowIndices.append(e) #the e-th equation
+							AcolIndiceappend(j*sourceWidth + i) #which entry
+							Adata.append(1) #v_i
+						
+							neighbor = source[j+n,i+m]
+						
+							#target image pixel value is a constant with respect to x, what we're solving for
+							b.append(neighbor - originalPixelValue + target[(j+n),(i+m)])
+							e += 1
+						
+	#shape of the matrix is (number of equations) x (number of pixels in source/target image)
+	A=scipy.sparse.csr_matrix((Adata,(ArowIndices,AcolIndices)),shape=(e,sourceWidth * sourceHeight))
+
+	#solve the least squares problem
+#	x = np.linalg.lstsq(A,b)[0]
+	x = scipy.sparse.linalg.lsqr(A,b)[0]
+	x = x.reshape(sourceArr.shape)
+	
+	print("x, the reconstruction: ")
+	print x
+	print("A, the equations: ")
+	print A
+	print("b, the solutions: ")
+	print b		
+
+						
 			
 	
 	'''
@@ -108,3 +161,8 @@ def poissonBlend(source, target, mask):
 	#Calculate the error
 	print 'Error: ',np.sum((imArr-x)**2)
 	'''
+if __name__ == "main":
+	source = np.matrix('1 2; 3 4')
+	target = np.matrix('3 1; 3 2')
+	mask = np.matrix('1 0; 0 0')
+	poissonBlend(source, target, mask)
