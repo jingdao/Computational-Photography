93e71197d832a3b3d61b6225659e4856a0592aa5
chenjingdao <chenjingdao@wustl.edu>
2015-03-09 18:26:38 -0500

    added more data sets

--- proj2/testPoissonBlending.py
+++ proj2/testPoissonBlending.py
@@ -2,18 +2,23 @@ from scipy import ndimage
 import numpy as np
 import poissonBlending
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 def testPoissonBlend(srcFile,tgtFile,maskFile,useMixedGradient):
 	src=ndimage.imread(srcFile)
 	tgt=ndimage.imread(tgtFile)
 	mask_img=ndimage.imread(maskFile)
 
+	#if mask img has multiple channels
+	if len(mask_img.shape)>2:
+		mask_img=mask_img[:,:,0]
+
 	#get mask: specifices which pixels in the source image are the source region
 	#True if pixel in source image is in source region, False otherwise
 	mask=np.zeros((mask_img.shape[0],mask_img.shape[1]),dtype=bool)
 	for i in range(mask_img.shape[0]):
 		for j in range(mask_img.shape[1]):
-			if np.sum(mask_img[i,j])>0:
+			if mask_img[i,j]>0:
 				mask[i,j]=True
 			else:
 				mask[i,j]=False
@@ -32,13 +37,26 @@ def testPoissonBlend(srcFile,tgtFile,maskFile,useMixedGradient):
 	tgt[:,:,0] = mask*vRed + (1 - mask)*tgt[:,:,0]
 	tgt[:,:,1] = mask*vGreen + (1 - mask)*tgt[:,:,1]
 	tgt[:,:,2] = mask*vBlue + (1 - mask)*tgt[:,:,2]
+
+	#this is the image with only values from the reconstructed source region
+	src2 = np.dstack([vRed,vGreen,vBlue])
 	
+	plt.figure()
+	plt.imshow(src2)
+	plt.figure()
+	plt.imshow(tgt[:,:,0],cmap=cm.Greys_r)
+	plt.figure()
+	plt.imshow(tgt[:,:,1],cmap=cm.Greys_r)
+	plt.figure()
+	plt.imshow(tgt[:,:,2],cmap=cm.Greys_r)
 	plt.figure()
 	plt.imshow(tgt)
 
 if __name__=="__main__":
-	useMixedGradient = False
+	useMixedGradient = True
 #	testPoissonBlend('samples/penguin_aligned.jpg','samples/im2_small.JPG','samples/penguin_mask.jpg',useMixedGradient)
+#	testPoissonBlend('samples/penguin_chick_aligned.jpg','samples/im2_small.JPG','samples/penguin_chick_mask.jpg',useMixedGradient)
 #	testPoissonBlend('samples/mona-leber-source.jpg','samples/mona-leber-target.jpg','samples/mona-leber-mask.jpg',useMixedGradient)
-	testPoissonBlend('samples/plane_src.jpg','samples/plane_tgt.jpg','samples/plane_mask.jpg',useMixedGradient)
+#	testPoissonBlend('samples/plane_src.jpg','samples/plane_tgt.jpg','samples/plane_mask.jpg',useMixedGradient)
+	testPoissonBlend('samples/mixed_src.jpg','samples/mixed_tgt.jpg','samples/mixed_mask.jpg',useMixedGradient)
 	plt.show()
