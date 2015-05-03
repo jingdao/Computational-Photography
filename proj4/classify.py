8e7e555b56558cc4b8cdc3758baf3ac9ba12b091
= <mheimann@umich.edu>
2015-05-03 11:30:54 -0500

    Now classifies images as real or fake

--- proj4/classify.py
+++ proj4/classify.py
@@ -1,40 +1,43 @@
 import numpy as np
 from sklearn import svm
 
-#compute features from raw images
-#takes in a matrix of raw images
-#returns a dxn matrix, where n is # of images (d features per image, all in a column)
-def compute_features(images):
-   pass
-
 #trains a machine learning classifier on a training data set
 #supervised: also takes in training labels
 #returns a classifier (a hypothesis)
 def train_classifier(training_data, training_labels):
-   pass
+   #support vector classification with linear kernel
+   classifier = svm.SVC()
+   classifier.fit(training_data, training_labels)
+   return classifier
 
 #test on test data
 #takes in a machine learning hypothesis and test data
 def test_classifier(classifier, test_data):
-   pass
+   predictions = classifier.predict(test_data)
+   return predictions
 
 #takes in a set of predictions and true labels and computes error
 #could be used to compute training or test error
 def compute_error(predictions, labels):
-   pass
+   differences = np.subtract(predictions,labels)
+   num_errors = np.count_nonzero(differences)
+   num_obs = predictions.size
+   error_rate = float(num_errors)/num_obs
+   return error_rate
 
 #take in images
 if __name__ == "__main__":
-   #sketch of process:
-   #images, labels, given (training and test)
-   #tr_data = compute_features(images)
-   #clf = train_classifier(tr_data,labels)
-   #test_preds = test_classifier(clf,test_images)
-   #print "Error: ", compute_error(test_preds,test_labels)
 
-	#load 2000 images, 1000 real and 1000 fake, each with 1000 features
-	#this is subdivided into 1600 for training data and 400 for test data
-	features_train = np.load('features_train.npy') #(1000,1600)
-	features_test = np.load('features_test.npy') #(1000,400)
-	labels_train =  np.load('labels_train.npy') #(1600)
-	labels_test = np.load('labels_test.npy') #(400,)
+   #load 2000 images, 1000 real and 1000 fake, each with 1000 features
+   #this is subdivided into 1600 for training data and 400 for test data
+   features_train = np.load('features_train.npy') #(1000,1600)
+   features_train = features_train.transpose()
+   features_test = np.load('features_test.npy') #(1000,400)
+   features_test = features_test.transpose()
+   labels_train =  np.load('labels_train.npy') #(1600)
+   labels_test = np.load('labels_test.npy') #(400,)i
+   
+   trained_classifier = train_classifier(features_train, labels_train)
+   test_predictions = test_classifier(trained_classifier, features_test)
+   test_error = compute_error(test_predictions, labels_test)
+   print("Error rate: %f" % test_error)
